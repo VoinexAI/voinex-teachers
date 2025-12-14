4e7d95395ec38d8fa06631c99f92adf82a8b694a
Kashish <voinex.in@gmail.com>
2025-12-14 11:11:17 -0800

    fix proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-export async function proxy(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl
   const token = request.cookies.get('authToken')?.value
   
